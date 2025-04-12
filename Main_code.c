de8620ff423e059ea31149812a7c9723629a5312
HenryNguyen23-FPT <nhatdang23092006@gmail.com>
2025-04-12 12:01:24 +0700

    Update Main_code.c

--- Main_code.c
+++ Main_code.c
@@ -24,14 +24,38 @@ void read_from_file() {
     if (pt == NULL) return;
 
     vehicle temp;
-    while (fscanf(pt, "%s %d %ld", temp.license_plate, &temp.fee, &temp.entry_time) == 3) {
-        if (num_vehicles < MAX_SLOTS) {
-            temp.clock_start = 0; // clock_start không được lưu trong file, đặt tạm là 0
-            vehicle_list[num_vehicles++] = temp;
+    char time_str[26]; // để đọc chuỗi thời gian
+
+    while (fscanf(pt, "%s %d %25[^\n]", temp.license_plate, &temp.fee, time_str) == 3) {
+        struct tm tm_time;
+        if (strptime(time_str, "%Y-%m-%d %H:%M:%S", &tm_time) != NULL) {
+            temp.entry_time = mktime(&tm_time);
+            temp.clock_start = 0; // reset clock_start
+            if (num_vehicles < MAX_SLOTS) {
+                vehicle_list[num_vehicles++] = temp;
+            }
         }
     }
     fclose(pt);
 }
+
+void log_action(const char *license_plate, const char *action) {
+    FILE *log = fopen("log.txt", "a");
+    if (!log) {
+        printf("Không thể mở log.txt để ghi!\n");
+        return;
+    }
+
+    time_t now = time(NULL);
+    struct tm *time_info = localtime(&now);
+
+    char time_str[30];
+    strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", time_info);
+
+    fprintf(log, "%s %s %s\n", license_plate, action, time_str);
+    fclose(log);
+}
+
 
 // Kiểm tra còn chỗ không
 int has_available_slot() {
@@ -88,7 +112,8 @@ void add_vehicle() {
     vehicle new_vehicle;
     printf("Note: License plate format should be 2 digits + 1 letter + '-' + 3 digits + '.' + 2 digits.\n");
     printf("Enter license plate: ");
-    scanf("%s", new_vehicle.license_plate);
+    fgets(new_vehicle.license_plate, sizeof(new_vehicle.license_plate), stdin);
+    new_vehicle.license_plate[strcspn(new_vehicle.license_plate, "\n")] = '\0'; // xóa kí tự '\n'
     Check_license_plate(new_vehicle.license_plate);
 
     for (int i = 0; i < num_vehicles; i++){
@@ -106,6 +131,8 @@ void add_vehicle() {
 
     printf("Stopwatch started for this vehicle.\n");
     printf("Vehicle added successfully!\n");
+    log_action(new_vehicle.license_plate, "in");
+	
 }
 
 void display_vehicle_list() {
@@ -151,6 +178,7 @@ void remove_vehicle(const char *license_plate) {
     printf("Elapsed time for vehicle %s: %02d:%02d:%02d (hh:mm:ss)\n", license_plate, hours, minutes, seconds);
     printf("Total fee: %d VND\n", veh->fee);
     Cal_total(veh->fee);
+    log_action(license_plate, "out");
 
     for (int i = 0; i < num_vehicles; i++) {
         if (strcmp(vehicle_list[i].license_plate, license_plate) == 0) {
@@ -176,27 +204,31 @@ void start_stopwatch() {
     int seconds = (int)elapsed_seconds % 60;
     printf("Elapsed time: %02d:%02d:%02d (hh:mm:ss)\n", hours, minutes, seconds);
 }
-void vehicle_sum(){
-	FILE *pt = fopen("parking_data.txt", "r");
+void vehicle_sum() {
+    FILE *pt = fopen("log.txt", "r");
     if (pt == NULL) {
-        printf("Cannot open file!\n");
+        printf("Không thể mở file log.txt!\n");
         return;
     }
-	int vehicle_sum_in = 0;
-	int vehicle_sum_out = 0;
-	char line[256];
-while (fgets(line, sizeof(line), pt)) {
-    if (strstr(line, "in") != NULL) {
-        vehicle_sum_in++;
-    } else if (strstr(line, "out") != NULL) {
-        vehicle_sum_out++;
+
+    int vehicle_sum_in = 0;
+    int vehicle_sum_out = 0;
+    char license[20], action[10];
+    char time_str[30];
+
+    while (fscanf(pt, "%s %s %[^\n]", license, action, time_str) == 3) {
+        if (strcmp(action, "in") == 0) {
+            vehicle_sum_in++;
+        } else if (strcmp(action, "out") == 0) {
+            vehicle_sum_out++;
+        }
     }
+
+    fclose(pt);
+    printf("Tổng số xe vào: %d\n", vehicle_sum_in);
+    printf("Tổng số xe ra : %d\n", vehicle_sum_out);
 }
 
-	fclose(pt);
-	printf("Tổng số xe vào: %d\n",vehicle_sum_in);
-	printf("Tổng số xe ra : %d\n",vehicle_sum_out);
-}
 void Cal_total(double fee){
 	total += fee;
 }//gọi hàm Cal_total(fee) ở trong hàm void remove_vehicle(const char *license_plate)
