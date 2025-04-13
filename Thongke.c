49a5a9979a9ec88b49534892ec7273d2e724c966
HenryNguyen23-FPT <nhatdang23092006@gmail.com>
2025-04-13 20:52:01 +0700

    Update Thongke.c

--- Thongke.c
+++ Thongke.c
@@ -3,28 +3,31 @@
 #include <math.h>
 double total=0; // biến toàn cục
 char vehicle_list[100];
-void vehicle_sum(){
-	FILE *pt = fopen("parking.txt", "r");
+void vehicle_sum() {
+    FILE *pt = fopen("log.txt", "r");
     if (pt == NULL) {
-        printf("Cannot open file!\n");
+        printf("Không thể mở file log.txt!\n");
         return;
     }
-	int vehicle_sum_in = 0;
-	int vehicle_sum_out = 0;
-	while (fgets(vehicle_list, sizeof(vehicle_list),pt)){
-		//kiem tra xe vao
-		if (strstr(vehicle_list, "in")!= NULL){
-			vehicle_sum_in++;
-		}
-		//kiem tra xe ra
-		else if (strstr(vehicle_list,"out")!= NULL){
-			vehicle_sum_out++;
-		}
-	}
-	fclose(pt);
-	printf("Tổng số xe vào: %d\n",vehicle_sum_in);
-	printf("Tổng số xe ra : %d\n",vehicle_sum_out);
+
+    int vehicle_sum_in = 0;
+    int vehicle_sum_out = 0;
+    char license[20], action[10];
+    char time_str[30];
+
+    while (fscanf(pt, "%s %s %[^\n]", license, action, time_str) == 3) {
+        if (strcmp(action, "in") == 0) {
+            vehicle_sum_in++;
+        } else if (strcmp(action, "out") == 0) {
+            vehicle_sum_out++;
+        }
+    }
+
+    fclose(pt);
+    printf("Tong so xe vao: %d\n", vehicle_sum_in);
+    printf("Tong so xe ra : %d\n", vehicle_sum_out);
 }
+
 void Cal_total(double fee){
 	total += fee;
 }//gọi hàm Cal_total(fee) ở trong hàm void remove_vehicle(const char *license_plate)
